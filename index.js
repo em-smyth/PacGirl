4357be18deef3e7f25e2a8f97b73467922dea9f9
Emma <105153833+em-smyth@users.noreply.github.com>
2023-07-08 21:16:46 +0100

    Created Power Up

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ class Ghost {
   draw() {
     c.beginPath();
     c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
-    c.fillStyle = this.color;
+    c.fillStyle = this.scared ? "orange" : this.color;
     c.fill();
     c.closePath();
   }
@@ -526,6 +526,25 @@ function animate() {
     }
   }
 
+  // detect collision between ghosts and plaer
+  for (let i = ghosts.length - 1; 0 <= i; i--) {
+    const ghost = ghosts[i];
+    // ghosts touches player
+    if (
+      Math.hypot(
+        ghost.position.x - player.position.x,
+        ghost.position.y - player.position.y
+      ) <
+      ghost.radius + player.radius
+    ) {
+      if (ghost.scared) {
+        ghosts.splice(i, 1);
+      } else {
+        cancelAnimationFrame(animationId);
+        console.log("You lose!");
+      }
+    }
+  }
   // Generate Power Up
   for (let i = powerUps.length - 1; 0 <= i; i--) {
     const powerUp = powerUps[i];
@@ -546,7 +565,7 @@ function animate() {
 
         setTimeout(() => {
           ghost.scared = false;
-        }, 3000);
+        }, 5000);
       });
     }
   }
@@ -583,17 +602,6 @@ function animate() {
 
   ghosts.forEach((ghost) => {
     ghost.update();
-
-    if (
-      Math.hypot(
-        ghost.position.x - player.position.x,
-        ghost.position.y - player.position.y
-      ) <
-      ghost.radius + player.radius
-    ) {
-      cancelAnimationFrame(animationId);
-      console.log("You lose!");
-    }
 
     const collisions = [];
     boundaries.forEach((boundary) => {
